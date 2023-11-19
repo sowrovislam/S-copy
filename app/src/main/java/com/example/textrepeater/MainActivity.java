c4d24f7178c64f545324eba460933d9ad06472ae
USER <sowrovnil5@gmail.com>
2023-11-20 02:31:07 +0600

    all android projact

--- app/src/main/java/com/example/textrepeater/MainActivity.java
+++ app/src/main/java/com/example/textrepeater/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.textrepeater;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
@@ -36,10 +41,55 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        materialToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                if (item.getItemId()==R.id.exit){
 
 
 
-//ata drawer layout ar kaj
+                }
+                return false;
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ata   Fragment
+
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout,new BlankFragment());
+        fragmentTransaction.commit();
+
+
+// ses   Fragment
+
+
+
+
+//>>>>>>>ata drawer layout ar kaj>>>>>>>>
 
         ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(
                 MainActivity.this,drawerLayout,materialToolbar,R.string.Drawer_close,R.string.Drawer_Open);
@@ -81,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+//>>>>>>>>>> ses dower layout >>>>>>
 
 
 
@@ -92,4 +142,44 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+        @Override
+        public void onBackPressed() {
+
+
+        new AlertDialog.Builder(MainActivity.this)
+
+                .setTitle("Thanks you ")
+                .setMessage("!! Do You Realy Exit")
+                .setIcon(R.drawable.img_1)
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        dialogInterface.dismiss();
+                    }
+                })
+                .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                        dialogInterface.dismiss();
+
+
+                    }
+                })
+
+
+                .show();
+
+
+
+
+
+
+
+    }
+
+
 }
